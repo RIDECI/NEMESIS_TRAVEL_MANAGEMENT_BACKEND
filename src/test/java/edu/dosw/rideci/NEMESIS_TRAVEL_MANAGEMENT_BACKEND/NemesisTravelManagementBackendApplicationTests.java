ae85834f023b5864cb295ea831dc078ba9c0cd5c
Tulio <tuliojr3101@gmail.com>
2025-11-08 20:27:10 -0500

    feat(core): Initial Project Structure

---
+++ src/test/java/edu/dosw/rideci/NEMESIS_TRAVEL_MANAGEMENT_BACKEND/NemesisTravelManagementBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.dosw.rideci.NEMESIS_TRAVEL_MANAGEMENT_BACKEND;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NemesisTravelManagementBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
