0183e0330657d850d29f6a9ba0f86a7b857e0e1c
Tulio <tuliojr3101@gmail.com>
2025-11-19 23:48:13 -0500

    feat(core): revisando propiedades añadir .env para seguridad de variables

--- src/main/java/edu/dosw/rideci/infrastructure/controller/TravelController.java
+++ src/main/java/edu/dosw/rideci/infrastructure/controller/TravelController.java
@@ -2,7 +2,6 @@ package edu.dosw.rideci.infrastructure.controller;
 
 import java.util.List;
 
-import org.apache.catalina.connector.Response;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
