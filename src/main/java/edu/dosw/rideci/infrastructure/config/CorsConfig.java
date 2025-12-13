e5a69fbabea6fd11ddda1a3db6eea974a0fcd9a3
Daniel <daniel.patino-m@mail.escuelaing.edu.co>
2025-12-12 19:34:01 -0500

    fix: Arreglando CorsConfig

--- src/main/java/edu/dosw/rideci/infrastructure/config/CorsConfig.java
+++ src/main/java/edu/dosw/rideci/infrastructure/config/CorsConfig.java
@@ -13,7 +13,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173", "https://rideci-frontend-n0y8ea9fl-danpame1219-9925s-projects.vercel.app")
+                        .allowedOrigins("http://localhost:5173", "https://rideci-nemesis.vercel.app")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*");
             }
