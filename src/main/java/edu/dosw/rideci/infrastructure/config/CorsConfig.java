d97eb26392a2822c01638587c3e3658a076f2dfc
Tulio <tuliojr3101@gmail.com>
2025-12-15 19:16:18 -0500

    fix(core): Ultimo despliegue

--- src/main/java/edu/dosw/rideci/infrastructure/config/CorsConfig.java
+++ src/main/java/edu/dosw/rideci/infrastructure/config/CorsConfig.java
@@ -13,7 +13,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173", "https://rideci-nemesis.vercel.app")
+                        .allowedOrigins("http://localhost:5173", "https://rideci.vercel.app")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*");
             }
